e178bd64e45b221eebf1a3044185a8a5522a828e
Lya Lopes <lyalopesc@gmail.com>
2026-04-18 23:19:57 -0300

    fix: corrigir imagem de perfil

--- src/components/layout/Hero/ProfileImage.tsx
+++ src/components/layout/Hero/ProfileImage.tsx
@@ -1,7 +1,9 @@
+import fotoPerfil from "../assets/fotoPerfil.png"
+
 export function ProfileImage() {
   return (
     <img
-      src="/src/assets/fotoPerfil.png"
+      src={fotoPerfil}
       alt="Foto de Perfil"
       className="w-full max-w-[18rem] md:max-w-[20rem] lg:max-w-md h-auto rounded-2xl shadow-lg border-2 border-purple-800"
     />
